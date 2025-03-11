edd5930ab65d6e1b225e3499b2d796539ca5d00e
chimdi ikechukwu <chimdiikechukwu00@gmail.com>
2025-03-11 03:20:11 +0100

    function update

--- tip.js
+++ tip.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     btn.addEventListener('click', function () {
       selectedPercentage = parseFloat(this.getAttribute('data-value'));
       customPercentage.value = '';
+      highlightSelectedBtn(this);
       calculateTip();
     });
   });
@@ -35,38 +36,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
   resetBtn.addEventListener('click', reset);
 
+  function highlightSelectedBtn(selectedBtn) {
+    document.querySelectorAll('.percentage-btn').forEach((btn) => {
+      btn.style.backgroundColor = '';
+      btn.style.color = '';
+
+      selectedBtn.style.backgroundColor = 'var(--Strong-cyan)';
+      selectedBtn.style.color = 'var(--Very-dark-cyan)';
+    });
+  }
+
   function calculateTip() {
     let billAmount = parseFloat(billInput.value);
     let numOfPeople = parseFloat(numberOfTippers.value);
 
-    if (numOfPeople) {
-      if (numOfPeople <= 0) {
-        errorMsg.style.display = 'block';
-        numberOfTippers.style.outline = '#ff0d0d';
-      } else {
-        errorMsg.style.display = 'none';
-      }
+    if (isNaN(numOfPeople) || numOfPeople <= 0) {
+      errorMsg.style.display = 'block';
+      numberOfTippers.style.outline = '2px solid #ff0d0d';
+      return;
+    } else {
+      errorMsg.style.display = 'none';
+      numberOfTippers.style.outline = '2px solid var(--Light-grayish-cyan)';
     }
 
     let percentageValue = (billAmount * (selectedPercentage || 0)) / 100;
 
     if (isNaN(billAmount) || isNaN(selectedPercentage)) return;
-    //divide bill into the amount of persons present
-
-    billIndividually.textContent = `$${(billInput / numOfPeople).toFixed(2)}`;
-
     //divide the tip perecntage to each person present
     tipIndividually.textContent = `$${(percentageValue / numOfPeople).toFixed(
       2
     )}`;
+
+    //divide bill into the amount of persons present
+    billIndividually.textContent = `$${(billAmount / numOfPeople).toFixed(2)}`;
+  }
+
+  function reset() {
+    billInput.value = '';
+    numberOfTippers.value = '';
+    billIndividually.textContent = '$0.00';
+    tipIndividually.textContent = '$0.00';
+    selectedPercentage = null;
+    customPercentage.value = '';
+    document.querySelectorAll('.percentage-btn').forEach((btn) => {
+      btn.style.backgroundColor = '';
+      btn.style.color = '';
+    });
   }
 });
-
-function reset() {
-  billInput.value = '';
-  numberOfTippers.value = '';
-  billIndividually.textContent = '$0.00';
-  tipIndividually.textContent = '$0.00';
-  selectedPercentage = null;
-  customPercentage.value = '';
-}
